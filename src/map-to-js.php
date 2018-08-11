a9049eadb369a2f1eea8507339cc4c4d92e014d8
suve <veg@svgames.pl>
2018-08-11 06:44:12 +0200

    Add the licensing header to the map converter

--- src/map-to-js.php
+++ src/map-to-js.php
@@ -1,5 +1,22 @@
 #!/usr/bin/php
 <?php
+/**
+ * LD42
+ * Copyright (C) 2018 Artur "suve" Iwicki
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program (LICENCE.txt). 
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
 
 function int_to_rgb(int $int) {
 	$b = $int & 0xFF;
